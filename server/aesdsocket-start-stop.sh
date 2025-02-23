731e318dece688b5be52184b40a14be3970637de
Bharath Varma <bhgo7033@colorado.edu>
2025-02-23 14:29:55 -0700

    added aesdsocket-start-stop.sh

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Startup script for aesdsocket daemon
+
+NAME=aesdsocket
+EXEC=" /usr/bin/aesdsocket" 
+echo $EXEC
+ARGS="-d"
+
+case "$1" in
+  start)
+    echo "Starting $NAME"
+    if [ ! -x "$EXEC" ]; then
+      echo "Error: $EXEC not found or not executable"
+      exit 1
+    fi
+    start-stop-daemon -S -n $NAME -a $EXEC -- $ARGS
+    ;;
+  stop)
+    echo "Stopping $NAME"
+    start-stop-daemon -K -n $NAME
+    ;;
+  *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
+
