c13d3cb361d7aaa399d772e842415a0026585498
Bharath Varma <bhgo7033@colorado.edu>
2025-03-16 16:31:47 -0600

    Local full test passed

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -8,6 +8,9 @@
 #ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
 #define AESD_CHAR_DRIVER_AESDCHAR_H_
 
+#include <linux/mutex.h>
+#include "aesd-circular-buffer.h"
+
 #define AESD_DEBUG 1  //Remove comment on this line to enable debug
 
 #undef PDEBUG             /* undef it, just in case */
@@ -29,6 +32,10 @@ struct aesd_dev
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
     struct cdev cdev;     /* Char device structure      */
+    struct aesd_circular_buffer circ_buffer;
+    struct mutex lock;
+    char *partial_write;
+    size_t partial_size;
 };
 
 
